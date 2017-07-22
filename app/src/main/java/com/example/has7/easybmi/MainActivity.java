97b791aa74574c42cbc48235a222c99ee494ae61
Pawan Modi <pmodi4@sapient.com>
2017-07-22 22:09:34 +0530

    Added location lat lng code

--- app/src/main/java/com/example/has7/easybmi/MainActivity.java
+++ app/src/main/java/com/example/has7/easybmi/MainActivity.java
@@ -1,17 +1,70 @@
 package com.example.has7.easybmi;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button locationButton;
+
+    Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationButton = (Button) findViewById(R.id.button3);
+
+        locationButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                fetchLocation(view);
+            }
+        });
     }
+
+    public void fetchLocation(View view) {
+        Log.d(this.getClass().getSimpleName(), "fetchLocation started");
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+        if (location == null) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Log.d(this.getClass().getSimpleName(), "Last Location: " + location);
+        }
+
+        TextView latlong = (TextView) findViewById(R.id.latlong);
+        latlong.setText(location.getLatitude() + " " + location.getLongitude());
+    }
+
+    // Define a listener that responds to location updates
+    LocationListener locationListener = new LocationListener() {
+        public void onLocationChanged(Location loc) {
+            // Called when a new location is found by the network location provider.
+            //makeUseOfNewLocation(location);
+            Log.d(this.getClass().getSimpleName(), "Lat-Log: " + location);
+            location = loc;
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+        public void onProviderEnabled(String provider) {}
+
+        public void onProviderDisabled(String provider) {}
+    };
 
     public void loadBMILayout(View view) {
         Intent intent = new Intent(this, BMIActivity.class);
